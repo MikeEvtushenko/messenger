b82ba6aee757afa34b63b96063275772edb66b33
Mike Evtushenko <57197670+MikeEvtushenko@users.noreply.github.com>
2021-04-22 09:41:44 +0300

    Create messenger.py

---
+++ messenger.py
@@ -0,0 +1,66 @@
+from PyQt6 import QtCore, QtGui, QtWidgets
+import clientui
+import requests
+from datetime import datetime
+
+
+class ExampleApp(QtWidgets.QMainWindow, clientui.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton.pressed.connect(self.send_message)
+        self.after = 0
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.get_messages)
+        self.timer.start(1000)
+
+    def show_messages(self, messages):
+        for message in messages:
+            dt = datetime.fromtimestamp(message["time"])
+            dt = dt.strftime("%H:%M")
+            self.textBrowser.append(dt + " " + message["name"])
+            self.textBrowser.append(message["text"])
+            self.textBrowser.append("")
+
+    def get_messages(self):
+        try:
+            response = requests.get(
+                "http://127.0.0.1:5000/messages",
+                params={"after": self.after}
+            )
+        except:
+            return
+
+        messages = response.json()["messages"]
+
+        if len(messages) > 0:
+            self.show_messages(messages)
+            self.after = messages[-1]["time"]
+
+
+    def send_message(self):
+        name = self.lineEdit.text()
+        text = self.textEdit.toPlainText()
+        try:
+            response = requests.post(
+                "http://127.0.0.1:5000/send",
+                json={"name": name, "text": text}
+            )
+        except:
+            self.textBrowser.append("Сервер недоступен")
+            self.textBrowser.append("")
+            return
+
+        if response.status_code != 200:
+            self.textBrowser.append("Cообщение не отправлено")
+            self.textBrowser.append("Проверьте имя и текст")
+            self.textBrowser.append("")
+            return
+        self.textEdit.clear()
+
+
+app = QtWidgets.QApplication([])
+window = ExampleApp()
+window.show()
+app.exec()
+
